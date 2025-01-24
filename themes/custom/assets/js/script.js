b1b621d1c49b53767f7638dc90496e162e7151c3
xDaveN <d.novotny97@protonmail.com>
2025-01-24 23:27:59 +0100

    fixed button styling

--- themes/custom/assets/js/script.js
+++ themes/custom/assets/js/script.js
@@ -7,10 +7,10 @@ document.querySelectorAll('.crypto-item').forEach(button => {
         try {
             const address = button.getAttribute('data-address');
             await navigator.clipboard.writeText(address);
-            const originalText = button.textContent;
-            button.textContent = "Address copied";
+            const originalText = button.querySelector('p').textContent;
+            button.querySelector('p').textContent = "Address copied";
             setTimeout(() => {
-                button.textContent = originalText;
+                button.querySelector('p').textContent = originalText;
             }, 3000);
         } catch (err) {
             console.error('Failed to copy text: ', err);
